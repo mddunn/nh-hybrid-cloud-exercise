690d775161f22f0b00423d1a59cf53388e7ec57c
Michael Dunn <mdunn@confluent.io>
2022-11-18 10:15:56 -0500

    updated Postgres tables and data

--- postgres/postgres-data.sql
+++ postgres/postgres-data.sql
@@ -30,19 +30,21 @@ create table if not exists grocery_shop.products (
     product_name varchar(40),
     product_cost money,
     product_quantity int,
-    seller_name varchar(40),
-    last_update_time timestamp NOT NULL
+    seller_id varchar(15),
+    last_update_time timestamp NOT NULL,
+    CONSTRAINT sellers FOREIGN KEY (seller_id) REFERENCES grocery_shop.sellers (seller_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 create table if not exists grocery_shop.orders (
     order_id integer,
-    customer_name varchar(15),
+    customer_id varchar(15),
     item_ordered integer,
     order_quantity integer,
     order_status grocery_shop.status,
     tracking_number varchar(20),
     create_time timestamp,
-    last_update_time timestamp NOT NULL
+    last_update_time timestamp NOT NULL,
+    CONSTRAINT customers FOREIGN KEY (customer_id) REFERENCES grocery_shop.customers (customer_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 -- Insert Customers
@@ -61,37 +63,37 @@ insert into grocery_shop.sellers (seller_id, seller_name, seller_company, seller
 
 
 -- Insert Products
-insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_name, last_update_time) values(1, 'Pork - Caul Fat', '$5.63', 19, '87-017-4611', '2022-07-11 18:58:42'::timestamp);
-insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_name, last_update_time) values(2, 'Tarragon - Primerba, Paste', '$27.94', 27, '51-894-3698', '2022-07-09 20:05:52'::timestamp);
-insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_name, last_update_time) values(3, 'Bagel - Ched Chs Presliced', '$18.01', 23, '79-156-2194', '2022-07-10 04:32:17'::timestamp);
-insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_name, last_update_time) values(4, 'V8 Splash Strawberry Kiwi', '$33.85', 46, '50-617-6068', '2022-07-08 12:52:26'::timestamp);
-insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_name, last_update_time) values(5, 'Potatoes - Yukon Gold, 80 Ct', '$33.92', 32, '35-950-3000', '2022-07-11 07:09:15'::timestamp);
+insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_id, last_update_time) values(1, 'Pork - Caul Fat', '$5.63', 16, '87-017-4611', '2022-07-11 18:58:42'::timestamp);
+insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_id, last_update_time) values(2, 'Tarragon - Primerba, Paste', '$27.94', 20, '51-894-3698', '2022-07-09 20:05:52'::timestamp);
+insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_id, last_update_time) values(3, 'Bagel - Ched Chs Presliced', '$18.01', 26, '79-156-2194', '2022-07-10 04:32:17'::timestamp);
+insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_id, last_update_time) values(4, 'V8 Splash Strawberry Kiwi', '$33.85', 16, '50-617-6068', '2022-07-08 12:52:26'::timestamp);
+insert into grocery_shop.products (product_id, product_name, product_cost, product_quantity, seller_id, last_update_time) values(5, 'Potatoes - Yukon Gold, 80 Ct', '$33.92', 12, '35-950-3000', '2022-07-11 07:09:15'::timestamp);
 
 -- Insert Orders
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', 'Alan Turing', 1, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 11:54:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', 'Alan Turing', 3, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 11:54:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', 'Alan Turing', 5, 2, 'SUBMITTED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 11:54:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', 'Bob Barker', 1, 2, 'SUBMITTED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 11:55:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', 'Bob Barker', 3, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 11:55:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', 'Chris Farley', 2, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 11:56:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', 'Chris Farley', 4, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 11:56:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', 'Luke Skywalker', 1, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:57:00'::timestamp, '2022-07-13 11:57:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', 'Ronald Reagan', 2, 3, 'SUBMITTED', 'N/A', '2022-07-13 11:58:00'::timestamp, '2022-07-13 11:58:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', 'Alan Turing', 1, 1, 'PROCESSED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 12:54:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', 'Alan Turing', 3, 1, 'PROCESSED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 12:54:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', 'Alan Turing', 5, 2, 'PROCESSED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 12:54:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', 'Bob Barker', 1, 2, 'PROCESSED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 12:55:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', 'Bob Barker', 3, 1, 'PROCESSED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 12:55:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', 'Chris Farley', 2, 1, 'PROCESSED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 12:56:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', 'Chris Farley', 4, 1, 'PROCESSED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 12:56:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', 'Luke Skywalker', 1, 1, 'PROCESSED', 'N/A', '2022-07-13 11:57:00'::timestamp, '2022-07-13 12:57:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', 'Ronald Reagan', 2, 3, 'PROCESSED', 'N/A', '2022-07-13 11:58:00'::timestamp, '2022-07-13 12:58:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', 'Chris Farley', 2, 1, 'IN-TRANSIT', '1349826936', '2022-07-13 11:56:00'::timestamp, '2022-07-14 11:56:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', 'Chris Farley', 4, 1, 'IN-TRANSIT', '1349826936', '2022-07-13 11:56:00'::timestamp, '2022-07-14 11:56:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', 'Luke Skywalker', 1, 1, 'IN-TRANSIT', '7616308716', '2022-07-13 11:57:00'::timestamp, '2022-07-14 11:57:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', 'Ronald Reagan', 2, 3, 'IN-TRANSIT', '4276596882', '2022-07-13 11:58:00'::timestamp, '2022-07-14 11:58:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', 'Luke Skywalker', 1, 1, 'SHIPPED', '7616308716', '2022-07-13 11:57:00'::timestamp, '2022-07-16 11:57:00'::timestamp);
-insert into grocery_shop.orders (order_id, customer_name, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', 'Ronald Reagan', 2, 3, 'SHIPPED', '4276596882', '2022-07-13 11:58:00'::timestamp, '2022-07-16 11:58:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', '53-570-6948', 1, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 11:54:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', '53-570-6948', 3, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 11:54:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', '53-570-6948', 5, 2, 'SUBMITTED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 11:54:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', '23-606-4767', 1, 2, 'SUBMITTED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 11:55:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', '23-606-4767', 3, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 11:55:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', '14-786-6861', 2, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 11:56:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', '14-786-6861', 4, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 11:56:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', '58-308-6113', 1, 1, 'SUBMITTED', 'N/A', '2022-07-13 11:57:00'::timestamp, '2022-07-13 11:57:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', '19-791-0773', 2, 3, 'SUBMITTED', 'N/A', '2022-07-13 11:58:00'::timestamp, '2022-07-13 11:58:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', '53-570-6948', 1, 1, 'PROCESSED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 12:54:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', '53-570-6948', 3, 1, 'PROCESSED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 12:54:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6706', '53-570-6948', 5, 2, 'PROCESSED', 'N/A', '2022-07-13 11:54:00'::timestamp, '2022-07-13 12:54:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', '23-606-4767', 1, 2, 'PROCESSED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 12:55:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6707', '23-606-4767', 3, 1, 'PROCESSED', 'N/A', '2022-07-13 11:55:00'::timestamp, '2022-07-13 12:55:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', '14-786-6861', 2, 1, 'PROCESSED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 12:56:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', '14-786-6861', 4, 1, 'PROCESSED', 'N/A', '2022-07-13 11:56:00'::timestamp, '2022-07-13 12:56:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', '58-308-6113', 1, 1, 'PROCESSED', 'N/A', '2022-07-13 11:57:00'::timestamp, '2022-07-13 12:57:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', '19-791-0773', 2, 3, 'PROCESSED', 'N/A', '2022-07-13 11:58:00'::timestamp, '2022-07-13 12:58:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', '14-786-6861', 2, 1, 'IN-TRANSIT', '1349826936', '2022-07-13 11:56:00'::timestamp, '2022-07-14 11:56:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6708', '14-786-6861', 4, 1, 'IN-TRANSIT', '1349826936', '2022-07-13 11:56:00'::timestamp, '2022-07-14 11:56:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', '58-308-6113', 1, 1, 'IN-TRANSIT', '7616308716', '2022-07-13 11:57:00'::timestamp, '2022-07-14 11:57:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', '19-791-0773', 2, 3, 'IN-TRANSIT', '4276596882', '2022-07-13 11:58:00'::timestamp, '2022-07-14 11:58:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6709', '58-308-6113', 1, 1, 'SHIPPED', '7616308716', '2022-07-13 11:57:00'::timestamp, '2022-07-16 11:57:00'::timestamp);
+insert into grocery_shop.orders (order_id, customer_id, item_ordered, order_quantity, order_status, tracking_number, create_time, last_update_time) values('6710', '19-791-0773', 2, 3, 'SHIPPED', '4276596882', '2022-07-13 11:58:00'::timestamp, '2022-07-16 11:58:00'::timestamp);
 
 select * from grocery_shop.customers;
 select * from grocery_shop.sellers;
